b06108eb15e24a05b43e1873a07cdf0851d90ece
Angel Melchor <angelsk8mw@gmail.com>
2020-04-05 10:32:57 +0200

    initial commit

--- dock_scheduler/models.py
+++ dock_scheduler/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 
 class Dock(models.Model):
@@ -35,3 +36,47 @@ class Dock(models.Model):
 
     def __str__(self):
         return f'Dock {self.number}'
+
+
+class TimeSegment(models.Model):
+    # foreign key
+    dock = models.ForeignKey(Dock, on_delete=models.CASCADE)
+    # segment
+    start_time = models.DateField()
+    end_time = models.DateField()
+    # activity
+    LOAD = 'CA'
+    UNLOAD = 'DE'
+    UNAVAILABLE = 'ND'
+    ACTIVITIES = [
+        (LOAD, 'Carga'),
+        (UNLOAD, 'Descarga'),
+        (UNAVAILABLE, 'No disponible'),
+    ]
+    activity = models.CharField(
+        max_length=2,
+        choices=ACTIVITIES)
+
+    def __str__(self):
+        return f'Dock {self.dock.number}, {self.activity} from {self.start_time} to {self.end_time}'
+
+
+class Booking(models.Model):
+    # foreign key
+    time_segment = models.ForeignKey(TimeSegment, on_delete=models.CASCADE)
+    # driver's license plate
+    driver = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f'{self.driver} at {str(self.time_segment)}'
+
+
+class Order(models.Model):
+    number = models.CharField(
+        primary_key=True,
+        max_length=6,
+        validators=[RegexValidator(r'^\d{1,10}$')],
+    )
+
+    def __str__(self):
+        return f'ORDER NO: {self.number}'
